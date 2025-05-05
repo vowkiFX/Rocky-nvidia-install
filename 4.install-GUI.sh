8461dfe7701e34290d0d98f45ca081b0f3b32475
vowkiFX <bdamkvist@gmail.com>
2025-05-05 23:18:28 +0200

    Add files via upload

---
+++ 4.install-GUI.sh
@@ -0,0 +1,13 @@
+sudo dnf install "alsa-lib compat-openssl11 dbus-libs expat fontconfig glibc libatomic libevent libglvnd-glx libglvnd-opengl libICE libSM libX11 libX11-xcb libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbcommon libxkbcommon-x11 libXrandr libXrender libXScrnSaver libXt libXtst libzstd nspr nss nss-util openldap pciutils-libs tbb xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm zlib"
+
+sudo dnf groupinstall "Server with GUI"
+systemctl set-default graphical
+sudo dnf install gnome-tweaks
+sudo subscription-manager repos --enable "codeready-builder-for-rhel-8-$(uname -m)-rpms
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum install epel-release
+
+echo "$APP_NAME has been installed, and the system has been updated. Press Enter to reboot into your system..."
+read
+# Reboot if you had a kernel update
+reboot
