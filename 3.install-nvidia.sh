47f0f5a48b0ac6cc59865a0201b2a276d9425c62
vowkiFX <bdamkvist@gmail.com>
2025-05-06 15:50:11 +0200

    Update 3.install-nvidia.sh

--- 3.install-nvidia.sh
+++ 3.install-nvidia.sh
@@ -10,3 +10,6 @@ echo -e "\033[0;32m=====================================\033[0m"
 # https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
 # install nvidia driver from nvidia's official repo
 sudo dnf module install nvidia-driver:latest-dkms -y
+sudo grubby --update-kernel=ALL --args="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau"
+sudo sed -i -e 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau /g' /etc/default/grub
+reboot
