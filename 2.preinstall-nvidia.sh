a9d5e25d9dbdc191b923a0c86063fda304997032
vowkiFX <bdamkvist@gmail.com>
2025-05-06 15:53:28 +0200

    Update 2.preinstall-nvidia.sh

--- 2.preinstall-nvidia.sh
+++ 2.preinstall-nvidia.sh
@@ -4,8 +4,9 @@ APP_NAME="epel-release and Official NVIDIA repo"
 
 # https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
 # rpmfusion-free-release and epel-release are part of extras
+sudo dnf config-manager --set-enabled crb
 sudo dnf install epel-release -y
-dnf config-manager --set-enabled crb
+
 # Get the major version and download the repo file
 curver="rhel$(rpm -E %rhel)"
 sudo wget -O /etc/yum.repos.d/cuda-$curver.repo \
