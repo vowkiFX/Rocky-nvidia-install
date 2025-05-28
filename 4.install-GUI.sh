3e7f46701a21142099cbfd79e2a845d998074140
vowkiFX <bdamkvist@gmail.com>
2025-05-28 19:53:50 +0200

    Update 4.install-GUI.sh

--- 4.install-GUI.sh
+++ 4.install-GUI.sh
@@ -1,9 +1,13 @@
 echo "houdini required packages"
 sudo dnf install alsa-lib compat-openssl11 dbus-libs expat fontconfig glibc libatomic libevent libglvnd-glx libglvnd-opengl libICE libSM libX11 libX11-xcb libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbcommon libxkbcommon-x11 libXrandr libXrender libXScrnSaver libXt libXtst libzstd nspr nss nss-util openldap pciutils-libs tbb xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm zlib
+sudo dnf -y install epel-release
+sudo dnf config-manager --set-enabled crb
+sudo dnf -y update
 
 sudo dnf groupinstall "KDE Plasma Workspaces" -y
-sudo dnf install kscreen sddm kde-gtk-config dolphin konsole kate plasma-discover firefox rocky-backgrounds sddm-breeze mpv  -y
-systemctl set-default graphical
+sudo systemctl disable gdm
+sudo dnf install kscreen sddm kde-gtk-config dolphin konsole kate plasma-discover firefox rocky-backgrounds sddm-breeze vlc -y
+sudo systemctl set-default graphical.target
 sudo systemctl enable sddm
 
 sudo dnf install flatpak
