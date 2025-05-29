d2cfd455c2997dafcb2f6b351dbf5ca4931b28ad
vowkiFX <bdamkvist@gmail.com>
2025-05-29 12:55:13 +0200

    Update 4.install-GUI.sh

--- 4.install-GUI.sh
+++ 4.install-GUI.sh
@@ -10,7 +10,7 @@ sudo systemctl set-default graphical.target
 sudo systemctl enable sddm
 
 sudo dnf install flatpak
-https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub io.missioncenter.MissionCenter
 flatpak install flathub de.z_ray.OptimusUI
 flatpak install flathub com.valvesoftware.Steam
