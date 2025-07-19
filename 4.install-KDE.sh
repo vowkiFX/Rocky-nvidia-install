1df3fb290e159e3d6929e1eb109f0c6c59b6fe02
vowkiFX <bdamkvist@gmail.com>
2025-07-20 00:22:18 +0200

    Update 4.install-KDE.sh

--- 4.install-KDE.sh
+++ 4.install-KDE.sh
@@ -1,11 +1,11 @@
 echo "houdini required packages"
-sudo dnf install alsa-lib compat-openssl11 dbus-libs expat fontconfig glibc libatomic libevent libglvnd-glx libglvnd-opengl libICE libSM libX11 libX11-xcb libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbcommon libxkbcommon-x11 libXrandr libXrender libXScrnSaver libXt libXtst libzstd nspr nss nss-util openldap pciutils-libs tbb xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm zlib
+sudo dnf install -y alsa-lib compat-openssl11 dbus-libs expat fontconfig glibc libatomic libevent libglvnd-glx libglvnd-opengl libICE libSM libX11 libX11-xcb libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbcommon libxkbcommon-x11 libXrandr libXrender libXScrnSaver libXt libXtst libzstd nspr nss nss-util openldap pciutils-libs tbb xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm zlib
 
 echo "davinci resolve required packages"
-sudo dnf install apr apr-util mesa-libGLU
+sudo dnf install apr apr-util mesa-libGLU -y
 
 
-sudo dnf -y install epel-release
+sudo dnf -y install epel-release -y
 
 sudo dnf config-manager --set-enabled crb
 
@@ -29,11 +29,11 @@ sudo hostnamectl set-hostname your-name
 
 
 
-sudo dnf install flatpak
+sudo dnf install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub io.missioncenter.MissionCenter
-flatpak install flathub it.mijorus.gearlever
-flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub io.missioncenter.MissionCenter -y
+flatpak install flathub it.mijorus.gearlever -y
+flatpak install flathub com.valvesoftware.Steam -y
 
 sudo reboot
